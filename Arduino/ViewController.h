1430c356c9fd1e6acd807100a20a05bed7da3220
Joshua Hecker <joshua@sierpinski.(none)>
2011-12-23 20:27:39 -0700

    Initial Commit

---
+++ Arduino/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Arduino
+//
+//  Created by Joshua Hecker on 12/23/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ArduinoTests/ArduinoTests.h
@@ -0,0 +1,13 @@
+//
+//  ArduinoTests.h
+//  ArduinoTests
+//
+//  Created by Joshua Hecker on 12/23/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ArduinoTests : SenTestCase
+
+@end
